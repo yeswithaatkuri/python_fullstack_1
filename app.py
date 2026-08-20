9f2060e294f9d3e50b4cdea47296287fcc515739
yeswitha.atkuri <yeswithaatkuri05@gmail.com>
2026-08-20 10:48:08 +0000

    updated env

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/courses")
+def courses():
+    return render_template("courses.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/trainer")
+def trainer():
+    return render_template("trainer.html")
+
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
